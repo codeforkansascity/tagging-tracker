1044a0a08bb4402afbc59652824de6a863f46039
Jake LaCombe <jake.lacombe2@gmail.com>
2017-09-23 12:54:24 -0500

    Add Navigation Bar to Application

--- index.ios.js
+++ index.ios.js
@@ -9,24 +9,67 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  NavigatorIOS,
+  TouchableHighlight,
+  StatusBar
 } from 'react-native';
 
+class MyView extends Component {
+  _handleBackPress() {
+    this.props.navigator.pop();
+  }
+
+  _handleNextPress(nextRoute) {
+    this.props.navigator.push(nextRoute);
+  }
+
+  render() {
+    const nextRoute = {
+      component: MyView,
+      title: 'Bar That',
+      passProps: { myProp: 'bar' }
+    };
+    return(
+      <TouchableHighlight>
+        <Text style={{marginTop: 200, alignSelf: 'center'}}>
+          See you on the other nav {this.props.myProp}!
+        </Text>
+      </TouchableHighlight>
+    );
+  }
+}
+
 export default class TaggingTracker extends Component {
+  _handleNavigationRequest() {
+    this.refs.nav.push({
+      component: MyView,
+      title: 'Tagging Tracker',
+      passProps: { myProp: 'genius' },
+    });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-          This is Jake LaCombe's react native app! This is testing the code!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View>
+        <StatusBar
+          backgroundColor="black"
+          barStyle="light-content"
+        />
+        <NavigatorIOS
+          ref='nav'
+          initialRoute={{
+            component: MyView,
+            title: 'Tagging Tracker',
+            passProps: { myProp: 'foo' },
+            rightButtonTitle: 'Add',
+            onRightButtonPress: () => this._handleNavigationRequest(),
+          }}
+          styles={styles}
+          barTintColor='#000000'
+          titleTextColor='#fff'
+          tintColor='#fff'
+        />
       </View>
     );
   }
