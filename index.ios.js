95fdda22c62b80ece506f75dd97369a84b7d5713
Jake LaCombe <jake.lacombe2@gmail.com>
2017-09-23 21:30:43 -0500

    Better Refine Camera Action

--- index.ios.js
+++ index.ios.js
@@ -11,10 +11,13 @@ import {
   Text,
   View,
   NavigatorIOS,
-  TouchableHighlight
+  TouchableHighlight,
+  StatusBar,
+  TabBarIOS
 } from 'react-native';
 
 import Camera from 'react-native-camera';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 class BadInstagramCloneApp extends Component {
   render() {
@@ -26,13 +29,17 @@ class BadInstagramCloneApp extends Component {
           }}
           style={cameraStyles.preview}
           aspect={Camera.constants.Aspect.fill}>
-          <Text style={cameraStyles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+          <Text style={{padding: 10, backgroundColor: '#000000', color: '#ffffff'}}onPress={this.takePicture.bind(this)}>
+            <Icon name="ios-aperture" size={30}/>
+          </Text>
         </Camera>
       </View>
     );
   }
 
   takePicture() {
+    alert('Picture Taken');
+
     const options = {};
     //options.location = ...
     this.camera.capture({metadata: options})
@@ -62,6 +69,14 @@ const cameraStyles = StyleSheet.create({
 });
 
 class MyView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'redTab',
+      notifCount: 0,
+      presses: 0,
+    };
+  }
   _handleBackPress() {
     this.props.navigator.pop();
   }
@@ -76,12 +91,24 @@ class MyView extends Component {
       title: 'Bar That',
       passProps: { myProp: 'bar' }
     };
+
     return(
-      <TouchableHighlight onPress={() => this._handleNextPress(nextRoute)}>
-        <Text style={{marginTop: 200, alignSelf: 'center'}}>
-          See you on the other nav {this.props.myProp}!
-        </Text>
-      </TouchableHighlight>
+      <View style={{flex: 1}}>
+        <TabBarIOS
+          unselectedTintColor="yellow"
+          tintColor="white"
+          barTintColor="black">
+         <Icon.TabBarItemIOS
+            iconName="ios-camera"
+            badge={this.state.notifCount > 0 ? this.state.notifCount : undefined}
+            selected={this.state.selectedTab === 'redTab'}
+            onPress={() => {
+              this._handleNextPress(nextRoute);
+            }}>
+              <Text></Text>
+          </Icon.TabBarItemIOS>
+        </TabBarIOS>
+      </View>
     );
   }
 }
@@ -89,13 +116,29 @@ class MyView extends Component {
 export default class TaggingTracker extends Component {
   render() {
     return (
-      <NavigatorIOS
-        initialRoute={{
-          component: MyView,
-          title: 'My Initial Scene',
-        }}
-        style={{flex: 1}}
-      />
+      <View style={{flex: 1}}>
+        <StatusBar
+          backgroundColor="blue"
+          barStyle="light-content"
+        />
+        <NavigatorIOS
+          initialRoute={{
+            component: MyView,
+            title: 'Current Tags',
+            statusBarHidden: true,
+          }}
+          style={{flex: 1}}
+          barTintColor="#000000"
+          tintColor="#ffffff"
+          titleTextColor="#ffffff"
+          renderScene={(route, navigator) =>
+            <View>
+              <StatusBar hidden={route.statusBarHidden} />
+              ...
+            </View>
+           }
+        />
+      </View>
     );
   }
 }
