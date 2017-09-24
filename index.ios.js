7d29ebeb3b6c6aec7f3506976655586f4ad52439
Jake LaCombe <jake.lacombe2@gmail.com>
2017-09-23 21:47:30 -0500

    Separate Components

--- index.ios.js
+++ index.ios.js
@@ -18,55 +18,7 @@ import {
 
 import Camera from 'react-native-camera';
 import Icon from 'react-native-vector-icons/Ionicons';
-
-class BadInstagramCloneApp extends Component {
-  render() {
-    return (
-      <View style={cameraStyles.container}>
-        <Camera
-          ref={(cam) => {
-            this.camera = cam;
-          }}
-          style={cameraStyles.preview}
-          aspect={Camera.constants.Aspect.fill}>
-          <Text style={{padding: 10, backgroundColor: '#000000', color: '#ffffff'}}onPress={this.takePicture.bind(this)}>
-            <Icon name="ios-aperture" size={30}/>
-          </Text>
-        </Camera>
-      </View>
-    );
-  }
-
-  takePicture() {
-    alert('Picture Taken');
-
-    const options = {};
-    //options.location = ...
-    this.camera.capture({metadata: options})
-      .then((data) => console.log(data))
-      .catch(err => console.error(err));
-  }
-}
-
-const cameraStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    color: '#000',
-    padding: 10,
-    margin: 40
-  }
-});
+import BadInstagramExample from './components/BadInstagramExample.js';
 
 class MyView extends Component {
   constructor(props) {
@@ -87,7 +39,7 @@ class MyView extends Component {
 
   render() {
     const nextRoute = {
-      component: BadInstagramCloneApp,
+      component: BadInstagramExample,
       title: 'Bar That',
       passProps: { myProp: 'bar' }
     };
