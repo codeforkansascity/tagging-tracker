463a6359a1b5900961c3fe0f95e651481cbdb2b7
Jake LaCombe <jake.lacombe2@gmail.com>
2017-09-24 15:36:05 -0500

    Add Geocoder to the form

--- components/NewTagData.js
+++ components/NewTagData.js
@@ -16,10 +16,13 @@ import {
 
 import Camera from 'react-native-camera';
 import Icon from 'react-native-vector-icons/Ionicons';
+import Geocoder from 'react-native-geocoding';
 
 import BaseView from './BaseView';
 
 const win = Dimensions.get('window');
+
+
 
 export default class NewTagData extends Component {
   constructor(props) {
@@ -28,16 +31,32 @@ export default class NewTagData extends Component {
       selectedTab: 'redTab',
       notifCount: 0,
       presses: 0,
+      neighborhood: ''
     };
   }
 
   componentWillMount() {
-    this.props.navigator.title = "Home";
+    navigator.geolocation.getCurrentPosition(this.getNeighborhoodCoordinates.bind(this), () => {}, {});
+  }
 
-    this.props.navigator.rightNavButton = {
-        text: "Button",
-        onPress: () => {alert('You submitted the form!')}
-    };
+  getNeighborhoodCoordinates(pos) {
+    Geocoder.setApiKey('AIzaSyD4Oan5v5glCpoKUNUN_AjckZ29YeETzV4');
+
+    Geocoder.getFromLatLng(pos.coords.latitude, pos.coords.longitude).then(
+      json => {
+        var address_component = json.results[0].address_components.find((component) => {
+          return component.types.includes('neighborhood');
+        });
+
+        if(address_component) {
+          this.setState({
+            neighborhood: address_component.long_name
+          });
+        }
+      },
+      error => {
+      }
+    );
   }
 
   submitForm() {
@@ -57,6 +76,8 @@ export default class NewTagData extends Component {
           <TextInput style={styles.input} defaultValue={''}/>
           <Text>Square Photage</Text>
           <TextInput style={styles.input} defaultValue={''} />
+          <Text>Neighborhood</Text>
+          <TextInput style={styles.input} value={this.state.neighborhood} />
           <Text>Describe the words of this Tag</Text>
           <TextInput multiline={true} style={styles.input} defaultValue={''} />
         </View>
@@ -82,6 +103,6 @@ const styles = StyleSheet.create({
     borderBottomColor: '#dddddd',
     marginBottom: 10,
     marginTop: 5,
-    color: '#0000ff'
+    color: '#0000ff',
   }
 });
