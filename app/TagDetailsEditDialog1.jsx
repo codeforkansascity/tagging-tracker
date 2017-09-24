def088accfe61fb58ae5bf307d5395d2ebfbce94
dv045327 <daniel.vu@cerner.com>
2017-09-24 10:20:45 -0500

    Added edit details dialog

---
+++ app/TagDetailsEditDialog1.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+  Text,
+} from 'react-native';
+
+class TagDetailsEditDialog extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+     return <Text>TagDetailsEditDialog</Text>;
+   }
+}
+
+export default TagDetailsEditDialog;
