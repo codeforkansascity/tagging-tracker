2a054d5dab742feb18167a337a1710b7858bee41
kol kheang <kol.kheang@gmail.com>
2017-10-01 16:50:23 -0500

    Fix error with GPS permission/crash

--- components/TagDetailsEditDialog.js
+++ components/TagDetailsEditDialog.js
@@ -27,72 +27,51 @@ export default class TagDetailsEditDialog extends Component {
 
     this.onAddressChange = this.onAddressChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    // this.getCurrentLatLong = this.getCurrentLatLong.bind(this);
+    this.getCurrentLatLong = this.getCurrentLatLong.bind(this);
+    this.getAddressFromLatLong = this.getAddressFromLatLong.bind(this);
   }
 
   componentDidMount() {
-
-    // this.getCurrentLatLong().then(coordinate => {
-    //   alert(coordinate);
-    //   // this.setState({
-    //   //   coordinate: {
-    //   //     latitude: coordinate.latitude,
-    //   //     longitude: coordinate.longitude,
-    //   //   }
-    //   // })
-    // });
-
-    this.getAddressFromLatLong(this.state.coordinate).then(address => {
-      this.setState({
-        address: address.formatted_address,
-        neighborhood: address.neighborhood,
-      })
-    });
+    this.getCurrentLatLong();
   }
 
   getCurrentLatLong() {
-    var options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    };
+      var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
 
-    function success(pos) {
-      const coordinate = pos.coords;
-      Promise.resolve(coordinate);
-    };
+      function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+      }
 
-    function error(err) {
-      console.warn(`ERROR(${err.code}): ${err.message}`);
-      Promise.reject('error');
-    };
-
-    return navigator.geolocation.getCurrentPosition(success, error, options);
+      navigator.geolocation.getCurrentPosition(this.getAddressFromLatLong.bind(this), error, options);
   }
 
-  getAddressFromLatLong(coords) {
-    return new Promise((resolve, reject) => {
-      Geocoder.getFromLatLng(coords.latitude, coords.longitude).then(
-        json => {
-          const formatted_address = json.results[0].formatted_address;
+  getAddressFromLatLong(pos) {
+    Geocoder.getFromLatLng(pos.coords.latitude, pos.coords.longitude).then(
+      json => {
+        const formatted_address = json.results[0].formatted_address;
 
-          let address_component = json.results[0].address_components.find((component) => {
-            return component.types.includes('neighborhood');
-          });
-          const neighborhood = address_component.long_name;
+        let address_component = json.results[0].address_components.find((component) => {
+          return component.types.includes('neighborhood');
+        });
+        const neighborhood = address_component.long_name;
 
-          const address = {
-            formatted_address: formatted_address,
-            neighborhood: neighborhood,
-          };
+        const address = {
+          formatted_address: formatted_address,
+          neighborhood: neighborhood,
+        };
 
-          resolve(address);
-        },
-        error => {
-          resolve('');
-        }
-      );
-    })
+        this.setState({
+          address: address.formatted_address,
+          neighborhood: address.neighborhood,
+        });
+      },
+      error => {
+      }
+    );
   }
 
   onAddressChange(address) {
